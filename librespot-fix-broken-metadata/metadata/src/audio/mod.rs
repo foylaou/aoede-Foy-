c625adee6cdbcfc5b178ed5b3a715eb21776b08d
Foy <s225002731@gmail.com>
2025-11-11 10:20:50 +0800

    update librespot 0.8.0

--- librespot-fix-broken-metadata/metadata/src/audio/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod file;
-pub mod item;
-
-pub use file::{AudioFileFormat, AudioFiles};
-pub use item::{AudioItem, UniqueFields};
--- librespot-fix-broken-metadata/protocol/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// This file is parsed by build.rs
-// Each included module will be compiled from the matching .proto definition.
-
-mod impl_trait;
-
-include!(concat!(env!("OUT_DIR"), "/mod.rs"));
